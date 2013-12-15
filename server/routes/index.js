5b907cb9c8b91b8eef6931109b3ee3053a0f41bc
Cesar Carruitero <cesar@mozilla.pe>
2013-12-14 23:28:08 -0500

    implement socket function

--- server/routes/index.js
+++ server/routes/index.js
@@ -122,4 +122,10 @@ module.exports = function (app, io) {
       });
     });
   });
+
+  io.sockets.on('connection', function (socket) {
+    socket.on('message', function (data) {
+        io.sockets.emit('message', data);
+    });
+  }
 }
